6d181a77cd84dee238c2fa1611f4516130d343ce
Phaysik <mmoore0111@gmail.com>
2023-10-07 05:16:41 -0500

    The beginning of the base repo

---
+++ Base/src/main.cpp
@@ -0,0 +1,19 @@
+/*! \file main.cpp
+    \brief C++ file for creating a main.
+    \details Contains the function definitions for creating a main
+    \date --/--/----
+    \version 1.0
+    \author Matthew Moore
+*/
+
+/*! \brief The entry point for the program
+    \date --/--/----
+    \version 1.0
+    \author Matthew Moore
+    \return int The status code of the program
+*/
+int main()
+{
+
+    return 0;
+}
