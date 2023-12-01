eb886a7f86c4db78ef4f636b2569753d03533e4a
Phaysik <mmoore0111@gmail.com>
2023-12-01 14:42:45 -0600

    Generalize conf.py

--- Base/conf.py
+++ Base/conf.py
@@ -3,14 +3,14 @@ from cgitb import html
 extensions = ["breathe"]
 
 master_doc = "rst/index"
-html_title = "Database Normalizer"
+html_title = "{project_name}"
 html_theme = "sphinx_book_theme"
 html_static_path = ["docs/_static/"]
 
 html_css_files = ["css/custom.css"]
 
 html_theme_options = {
-    "repository_url": "https://github.com/Phaysik/database-normalizer",
+    "repository_url": "{github_repository_url}",
     "use_repository_button": True,
     "home_page_in_toc": True,
 }
