5b1dba695bbad7257e57d2b8db43d64ce7c43ee2
phaysik <mmoore0111@gmail.com>
2026-04-22 14:54:59 -0700

    Able to switch on demand between GoogleTest and Catch2

--- Base/tests/main/testMain.cpp
+++ Base/tests/main/testMain.cpp
@@ -5,10 +5,22 @@
     @author Matthew Moore
 */
 
-#include "gtest/gtest.h"
+#ifdef catch2
+	#include <catch2/catch_session.hpp>
+#elifdef googletest
+	#include <gtest/gtest.h>
+#else
+	#error "No testing framework defined. Please define either catch2 or googletest."
+#endif
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+#ifdef catch2
+	return Catch::Session().run(argc, argv);
+#elifdef googletest
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+#else
+	#error "No testing framework defined. Please define either catch2 or googletest."
+#endif
 }
