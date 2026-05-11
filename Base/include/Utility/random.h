83d3c86bf9a094213ae7f40f773005ef30596e5d
phaysik <mmoore0111@gmail.com>
2026-05-11 09:20:19 -0700

    Update trailing commas

--- Base/include/Utility/random.h
+++ Base/include/Utility/random.h
@@ -83,7 +83,8 @@ namespace Project::Utility
 					randomDevice(),
 					randomDevice(),
 					randomDevice(),
-					randomDevice()};
+					randomDevice(),
+				};
 
 				return std::mt19937{seedSequence};
 			}
